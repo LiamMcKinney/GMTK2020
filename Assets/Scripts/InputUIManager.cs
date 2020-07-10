c70e5e54ee846c4f81483273edea9e5f32b6d5b4
LiamMcKinney <ljmckinney02@gmail.com>
2020-07-10 18:26:49 -0500

    half did everything

---
+++ Assets/Scripts/InputUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputUIManager : MonoBehaviour
+{
+    public List<string> controlNames;
+    public List<InputField> controlBoxes;
+
+    public int numControls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for(int i)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnChangeControl(string controlIndex)
+    {
+
+    }
+
+    public void Hi(string hello)
+    {
+        print(hello);
+    }
+}
---
+++ Assets/Scripts/Keybind.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keybind : MonoBehaviour
+{
+    BoxCollider2D coll;
+    bool dragging;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (coll.OverlapPoint(Input.mousePosition))
+        {
+            dragging = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
+}
