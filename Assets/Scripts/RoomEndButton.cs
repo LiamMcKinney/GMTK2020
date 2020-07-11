d224bac29f8896694095b3e26b5c30228b08bbec
LiamMcKinney <ljmckinney02@gmail.com>
2020-07-10 21:40:16 -0500

    tiiiles

--- Assets/Scripts/RoomEndButton.cs
+++ Assets/Scripts/RoomEndButton.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RoomEndButton : MonoBehaviour
 {
+    public List<Interactable> enemies = new List<Interactable>();
+    public List<Interactable> roomElements = new List<Interactable>();
+
     // Start is called before the first frame update
     void Start()
     {
