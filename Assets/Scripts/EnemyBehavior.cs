5b364f77df2b721fbec0dfca1555b3eb0601be67
Nithead <nithead@yahoo.com>
2020-07-10 20:52:49 -0500

    aggressive enemy follows the player

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class EnemyBehavior : Interactable
 {
+    public float speed;
+    public PlayerBehavior player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(speed == 0)
+        {
+            speed = .015f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 target = player.transform.position;
+        Vector3 oldPos = transform.position;
+        float xMotion = 0;
+        float yMotion = 0;
+        if (target.x > oldPos.x)
+        {
+            xMotion = speed;
+        }
+        else if (target.x < oldPos.x)
+        {
+            xMotion = -speed;
+        }
+        if (target.y > oldPos.y)
+        {
+            yMotion = speed;
+        }
+        else if (target.y < oldPos.y)
+        {
+            yMotion = -speed;
+        }
+        transform.position = new Vector3(xMotion, yMotion, 0) + oldPos;
     }
 
     public override void OnHit()
