807268045324342d439239fd98e5ec0540509dd3
LiamMcKinney <ljmckinney02@gmail.com>
2020-07-12 00:41:57 -0500

    Title screen

---
+++ Assets/Scripts/Input and UI/TitleScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Sprite[] sprites = new Sprite[2];
+    int counter;
+    int index;
+    public int animFrames;
+
+    public Image player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if(counter > animFrames)
+        {
+            counter = 0;
+            index = (index + 1) % 2;
+            player.sprite = sprites[index];
+        }
+
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+    }
+}
