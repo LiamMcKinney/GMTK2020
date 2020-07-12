c58133750cbe36120b989469a7ed52c693dc96d3
Nithead <nithead@yahoo.com>
2020-07-12 08:04:07 -0500

    winscreen text

---
+++ Assets/Scripts/Room Elements/WinScreenText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreenText : MonoBehaviour
+{
+    public WinScreen winScreen;
+    public bool floor2Exists;
+    public Text text;
+    public bool endlessExists;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Congratulations, you win!\n Percentage of Tutorial Machines Repaired: " + winScreen.tutorialCompletion + "\n Percentage of 1st Floor Machines Repaired: " + winScreen.floor1Completion;
+        if (floor2Exists)
+        {
+            text.text += "\n Percentage of 2nd Floor Machines Repaired: " + winScreen.floor2Completion;
+        }
+        if (endlessExists)
+        {
+            text.text += "\nConsider Trying Endless Mode";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
